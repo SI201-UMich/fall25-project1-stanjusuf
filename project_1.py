18c03fd1212b691c7dca248b0bbc31c09a6aa09a
Stanford Jusuf <sjusuf@umich.edu>
2025-10-15 17:09:37 -0400

    Completed the group_by_year_island_species_sex(penguin_data) function

--- project_1.py
+++ project_1.py
@@ -5,22 +5,60 @@
 import csv
 
 def load_penguin_data(csv_filepath):
-    penguin_data = []
+    penguin_data = {}
+
     with open(csv_filepath, 'r', newline='') as csvfile:
         csv_reader = csv.reader(csvfile)
+        headers = next(csv_reader)
         for row in csv_reader:
-            penguin_data.append(row)
+            key = row[0]
+            values = {headers[i]: row[i] for i in range(1, len(row))}
+            penguin_data[key] = values
+
     return penguin_data
 
-def characterize_penguin_data(penguin_data):
-    print(f"Variables: {penguin_data[0]}")
-    print(f"Sample entry: {penguin_data[3]}")
-    print(f"Number of rows: {len(penguin_data) - 1}")
-    return 
+def group_weight_by_year_island_species_sex(penguin_data):
+    grouped_penguin_data = {}
+
+    for _, record in penguin_data.items():
+        year = record.get("year")
+        island = record.get("island")
+        species = record.get("species")
+        sex = record.get("sex")
+        weight = record.get("body_mass_g")
+        if not weight or not weight.isdigit():
+            continue
+        weight = int(weight)
+        if year not in grouped_penguin_data:
+            grouped_penguin_data[year] = {}
+        if island not in grouped_penguin_data[year]:
+            grouped_penguin_data[year][island] = {}
+        if species not in grouped_penguin_data[year][island]:
+            grouped_penguin_data[year][island][species] = {}
+        if sex not in grouped_penguin_data[year][island][species]:
+            grouped_penguin_data[year][island][species][sex] = []
+        grouped_penguin_data[year][island][species][sex].append(weight)
+
+    return grouped_penguin_data
+
+def mean_weight_by_sex(grouped_penguin_data):
+    pass
+
+def median_weight_by_sex(grouped_penguin_data):
+    pass
+
+def mode_meight_by_sex(grouped_penguin_data):
+    pass
+
+def distribution_shapes(means, medians, modes):
+    pass
+
+def generate_report(distribution_shapes):
+    pass
 
 def main():
     penguin_data = load_penguin_data('penguins.csv')
-    characterize_penguin_data(penguin_data)
+    print(penguin_data)
 
 
 if __name__ == "__main__":
